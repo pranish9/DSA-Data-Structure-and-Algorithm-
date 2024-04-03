483954b58d6939a7bad72e4ec26848147573d5bc
pranish9 <pranishtuladhar9@gmail.com>
2024-04-03 17:12:33 +0545

    binary serach practice

--- DSA_javaPranish/src/FiveChapter/BinarySearch.java
+++ DSA_javaPranish/src/FiveChapter/BinarySearch.java
@@ -1,22 +1,21 @@
 package FiveChapter;
-
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr={1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int target = 18;
-        int size = arr.length;
+        int[] sortedArray ={1, 5, 6, 7, 9, 11, 15, 16};
+        int target = 9;
+        int size = sortedArray.length;
         int left = 0;
         int right = size - 1;
         boolean found = false;
         while(left<=right){
             int mid = (left + right) / 2;
-            if(arr[mid] == target){
-                System.out.println("the traget is found in"+ mid);
+            if(sortedArray [mid] == target){
+                System.out.println("the traget is found in "+ mid);
                 found = true;
                 break;
-            } else if(arr[mid]>target){
+            } else if(sortedArray [mid]>target){
                 right = mid - 1;
-            } else if (arr [mid]<target){
+            } else if (sortedArray [mid]<target){
                 left = mid + 1;
             }
         }
